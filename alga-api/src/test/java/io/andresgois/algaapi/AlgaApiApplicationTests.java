2059da937839babfc876fbd36de0aa640af93c47
Andre Gois <andresousalee@gmail.com>
2025-04-21 18:18:41 -0300

    :tada: Commit inicial - projeto 01

---
+++ alga-api/src/test/java/io/andresgois/algaapi/AlgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.andresgois.algaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
