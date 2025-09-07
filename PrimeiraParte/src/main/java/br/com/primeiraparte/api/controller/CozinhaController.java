a57ea65c18d00b135927515d9fddf3508384a772
andre <andresousalee@gmail.com>
2025-09-07 19:32:48 -0300

    Negociando o media type do payload do POST com Content-Type

--- PrimeiraParte/src/main/java/br/com/primeiraparte/api/controller/CozinhaController.java
+++ PrimeiraParte/src/main/java/br/com/primeiraparte/api/controller/CozinhaController.java
@@ -3,10 +3,11 @@ package br.com.primeiraparte.api.controller;
 import br.com.primeiraparte.domain.entity.Cozinha;
 import br.com.primeiraparte.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +20,18 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhasRepository.listar();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        Cozinha cozinha = cozinhasRepository.buscar(id);
+        return ResponseEntity.ok(cozinha);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+        cozinhasRepository.salvar(cozinha);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cozinha.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
