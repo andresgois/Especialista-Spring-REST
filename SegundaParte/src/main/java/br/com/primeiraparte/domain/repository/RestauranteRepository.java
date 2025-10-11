fd79ae63cb44da4dd92900cd607332eb809301d1
andre <andresousalee@gmail.com>
2025-10-10 23:04:28 -0300

    5.8. Conhecendo os prefixos de query methods

--- SegundaParte/src/main/java/br/com/primeiraparte/domain/repository/RestauranteRepository.java
+++ SegundaParte/src/main/java/br/com/primeiraparte/domain/repository/RestauranteRepository.java
@@ -4,7 +4,17 @@ import br.com.primeiraparte.domain.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    boolean existsByNome(String nome);
+    Optional<Restaurante> findByNome(String nome);
+    Optional<Restaurante> findById(Long id);
+    Optional<Restaurante> findByNomeContaining(String nome);
+    List<Restaurante> findAllByNomeContaining(String nome);
+    List<Restaurante> findTop3ByNomeContaining(String nome);
+    int countByNomeContaining(String nome);
 }
