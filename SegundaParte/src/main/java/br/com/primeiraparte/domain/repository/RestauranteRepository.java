2d91dc927cb02839ea67e4a8b40dbcb970b28edf
andre <andresousalee@gmail.com>
2025-10-10 23:08:42 -0300

    5.9. Usando queries JPQL customizadas com @Query

--- SegundaParte/src/main/java/br/com/primeiraparte/domain/repository/RestauranteRepository.java
+++ SegundaParte/src/main/java/br/com/primeiraparte/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package br.com.primeiraparte.domain.repository;
 
 import br.com.primeiraparte.domain.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findAllByNomeContaining(String nome);
     List<Restaurante> findTop3ByNomeContaining(String nome);
     int countByNomeContaining(String nome);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(@Param("nome") String nome, @Param("id") String id);
 }
