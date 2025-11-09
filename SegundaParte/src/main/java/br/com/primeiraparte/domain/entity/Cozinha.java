7ce95f1770031cb59b3966acc1c63994321596e4
andre <andresousalee@gmail.com>
2025-11-09 15:33:20 -0300

    6.1. Mapeando relacionamento bidirecional com @OneToMany

--- SegundaParte/src/main/java/br/com/primeiraparte/domain/entity/Cozinha.java
+++ SegundaParte/src/main/java/br/com/primeiraparte/domain/entity/Cozinha.java
@@ -1,8 +1,12 @@
 package br.com.primeiraparte.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -17,5 +21,9 @@ public class Cozinha {
 
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
