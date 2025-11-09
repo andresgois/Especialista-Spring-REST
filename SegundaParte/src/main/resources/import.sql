3555a5e345f08d0209da242cd75c9f7f2952a78b
andre <andresousalee@gmail.com>
2025-11-09 16:50:53 -0300

    6.4. Mapeando classes incorporáveis com @Embedded e @Embeddable

--- SegundaParte/src/main/resources/import.sql
+++ SegundaParte/src/main/resources/import.sql
@@ -1,13 +1,8 @@
 INSERT INTO tb_users (id, name, age, password, login,email, ativado) VALUES (UUID(),'Ana Silva', 30, 'senha123', 'ana.silva','ana@email.com','F');
 INSERT INTO tb_users (id, name, age, password, login,email, ativado) VALUES (UUID(),'João Santos', 25, 'senha456', 'joao.santos','joao@email.com','F');
 
-
 insert into tb_cozinha (id, nome) values (1,'Tailandesa');
 insert into tb_cozinha (id, nome) values (2,'Indiana');
-
-insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10,1);
-insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50,2);
-insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15,1);
 
 INSERT INTO tb_estado (nome) VALUES ('Acre');
 INSERT INTO tb_estado (nome) VALUES ('Alagoas');
@@ -37,6 +32,14 @@ INSERT INTO tb_estado (nome) VALUES ('São Paulo');
 INSERT INTO tb_estado (nome) VALUES ('Sergipe');
 INSERT INTO tb_estado (nome) VALUES ('Tocantins');
 
-INSERT INTO db_processo.tb_forma_pagamento (descricao) VALUES ('Cartão de crédito'), ('Cartão de débito'), ('Dinheiro');
+INSERT INTO tb_cidade (estado_id, id, nome) VALUES(6, 1, 'Maranguape');
 
-INSERT INTO db_processo.restaurante_forma_pagamento (forma_pagamento_id, restaurante_id) VALUES (1,1),(1,2),(1,3),(2,1),(1,2),(2,3),(3,1),(3,2);
+insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10,1);
+insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50,2);
+insert into tb_restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15,1);
+
+INSERT INTO tb_restaurante (taxa_frete, cozinha_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, nome) VALUES (7, 1, 1, 'Novo Mpe', '61942250', 'Bloco A', 'Rua Nove', '360', 'Brasilis');
+
+INSERT INTO tb_forma_pagamento (descricao) VALUES ('Cartão de crédito'), ('Cartão de débito'), ('Dinheiro');
+
+INSERT INTO restaurante_forma_pagamento (forma_pagamento_id, restaurante_id) VALUES (1,1),(1,2),(1,3),(2,1),(1,2),(2,3),(3,1),(3,2);
