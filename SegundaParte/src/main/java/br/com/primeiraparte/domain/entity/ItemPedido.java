64dacf19f62b447c0fb5fe5cfaa720abe1deaf0c
Andre Gois <andresousalee@gmail.com>
2026-02-07 13:35:20 -0300

    Renamed tables and created Pedido and ItemPedido entities with repositories

---
+++ SegundaParte/src/main/java/br/com/primeiraparte/domain/entity/ItemPedido.java
@@ -0,0 +1,37 @@
+package br.com.primeiraparte.domain.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "tb_item_pedido")
+public class ItemPedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private BigDecimal precoUnitario;
+	private BigDecimal precoTotal;
+	private Integer quantidade;
+	private String observacao;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Pedido pedido;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+
+}
---
+++ SegundaParte/src/main/java/br/com/primeiraparte/domain/repository/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package br.com.primeiraparte.domain.repository;
+
+import br.com.primeiraparte.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+
+}
